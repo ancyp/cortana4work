69e75c3354e2c67eb7a8edb730066e436c736066
Shloak Jain <shloak@gmail.com>
2018-07-24 17:15:24 -0700

    skill add task

--- parsing-service/skill_addevent.py
+++ parsing-service/skill_addevent.py
@@ -4,8 +4,7 @@ from flask import Flask, Response, request, json
 import uuid
 import json
 
-#token = 'eyJ0eXAiOiJKV1QiLCJub25jZSI6IkFRQUJBQUFBQUFEWHpaM2lmci1HUmJEVDQ1ek5TRUZFZ0duTHNUQVRxM0VYVW9IZkVpU0E0THp4TnE5dW9DaHgyZnlWTjhHNmFDOTVaSUZuWDV2dFpKRG5UNWlHVVFoYlNnYnltWkJfb3R3R0M4UlB6c2FLb0NBQSIsImFsZyI6IlJTMjU2IiwieDV0IjoiN19adWYxdHZrd0x4WWFIUzNxNmxValVZSUd3Iiwia2lkIjoiN19adWYxdHZrd0x4WWFIUzNxNmxValVZSUd3In0.eyJhdWQiOiJodHRwczovL2dyYXBoLm1pY3Jvc29mdC5jb20iLCJpc3MiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC83MmY5ODhiZi04NmYxLTQxYWYtOTFhYi0yZDdjZDAxMWRiNDcvIiwiaWF0IjoxNTMyMzkwNjE2LCJuYmYiOjE1MzIzOTA2MTYsImV4cCI6MTUzMjM5NDUxNiwiYWNjdCI6MCwiYWNyIjoiMSIsImFpbyI6IkFVUUF1LzhJQUFBQTlQUW9yTlBJdkVTbi9wUktWRFhlYlFPbzd4MjhOWXlIaWtiT2tGTEJtL21teHllRTVkL1lhem1LZTlIUTlvRW9sVCt5WHAwenEyS09DblN5U1RnTUxRPT0iLCJhbXIiOlsid2lhIiwibWZhIl0sImFwcF9kaXNwbGF5bmFtZSI6IkdyYXBoIGV4cGxvcmVyIiwiYXBwaWQiOiJkZThiYzhiNS1kOWY5LTQ4YjEtYThhZC1iNzQ4ZGE3MjUwNjQiLCJhcHBpZGFjciI6IjAiLCJmYW1pbHlfbmFtZSI6IkphaW4iLCJnaXZlbl9uYW1lIjoiU2hsb2FrIiwiaW5fY29ycCI6InRydWUiLCJpcGFkZHIiOiIxMzEuMTA3LjE2MC41MiIsIm5hbWUiOiJTaGxvYWsgSmFpbiIsIm9pZCI6IjVlYTIwYzJhLTNhODctNDE5ZC1hOWRjLWY3MzlmMjZiMGRmNyIsIm9ucHJlbV9zaWQiOiJTLTEtNS0yMS0yMTI3NTIxMTg0LTE2MDQwMTI5MjAtMTg4NzkyNzUyNy0zMTY4MjEwOSIsInBsYXRmIjoiMyIsInB1aWQiOiIxMDAzMDAwMEFBQkZGRTE5Iiwic2NwIjoiQ2FsZW5kYXJzLlJlYWRXcml0ZSBDb250YWN0cy5SZWFkV3JpdGUgRGV2aWNlTWFuYWdlbWVudEFwcHMuUmVhZC5BbGwgRGV2aWNlTWFuYWdlbWVudEFwcHMuUmVhZFdyaXRlLkFsbCBEZXZpY2VNYW5hZ2VtZW50Q29uZmlndXJhdGlvbi5SZWFkLkFsbCBEZXZpY2VNYW5hZ2VtZW50Q29uZmlndXJhdGlvbi5SZWFkV3JpdGUuQWxsIERldmljZU1hbmFnZW1lbnRNYW5hZ2VkRGV2aWNlcy5Qcml2aWxlZ2VkT3BlcmF0aW9ucy5BbGwgRGV2aWNlTWFuYWdlbWVudE1hbmFnZWREZXZpY2VzLlJlYWQuQWxsIERldmljZU1hbmFnZW1lbnRNYW5hZ2VkRGV2aWNlcy5SZWFkV3JpdGUuQWxsIERldmljZU1hbmFnZW1lbnRSQkFDLlJlYWQuQWxsIERldmljZU1hbmFnZW1lbnRSQkFDLlJlYWRXcml0ZS5BbGwgRGV2aWNlTWFuYWdlbWVudFNlcnZpY2VDb25maWcuUmVhZC5BbGwgRGV2aWNlTWFuYWdlbWVudFNlcnZpY2VDb25maWcuUmVhZFdyaXRlLkFsbCBEaXJlY3RvcnkuQWNjZXNzQXNVc2VyLkFsbCBEaXJlY3RvcnkuUmVhZFdyaXRlLkFsbCBGaWxlcy5SZWFkV3JpdGUuQWxsIEdyb3VwLlJlYWRXcml0ZS5BbGwgSWRlbnRpdHlSaXNrRXZlbnQuUmVhZC5BbGwgTWFpbC5SZWFkV3JpdGUgTWFpbGJveFNldHRpbmdzLlJlYWRXcml0ZSBOb3Rlcy5SZWFkV3JpdGUuQWxsIG9wZW5pZCBQZW9wbGUuUmVhZCBwcm9maWxlIFJlcG9ydHMuUmVhZC5BbGwgU2l0ZXMuUmVhZFdyaXRlLkFsbCBUYXNrcy5SZWFkV3JpdGUgVXNlci5SZWFkQmFzaWMuQWxsIFVzZXIuUmVhZFdyaXRlIFVzZXIuUmVhZFdyaXRlLkFsbCBlbWFpbCIsInNpZ25pbl9zdGF0ZSI6WyJrbXNpIl0sInN1YiI6IlNyNm51V0N1T2NiQ3U3MnhYY2xfM051RlB4WFNhc05KZVFUd2hiNjZJdnMiLCJ0aWQiOiI3MmY5ODhiZi04NmYxLTQxYWYtOTFhYi0yZDdjZDAxMWRiNDciLCJ1bmlxdWVfbmFtZSI6InQtc2hqYUBtaWNyb3NvZnQuY29tIiwidXBuIjoidC1zaGphQG1pY3Jvc29mdC5jb20iLCJ1dGkiOiJEbHYyUmNQS3gwYWljUi1CeG9BQ0FBIiwidmVyIjoiMS4wIiwieG1zX3N0Ijp7InN1YiI6ImhTUk1Dd3NneGdJMVV4M0NNMC1HVnRIODlDX0FublhYdmcxUHExOGo3cjAifX0.UQLkiqlYybEqQ0RDcCH_-6GH1BwycFeBnonFgS2zngP42ykt5zDgv2qwUwvPjYK78Dxe5TfM_7-W7Zs_wiXUjrXo0f4OY8PqBEKtPc0BTzGx8oDpZXiC_-zb5acq5NQX_yYdXyc5S91gkO5KJ3_BQXI7gqK1gmmSnnQm3kJV6TQdnmIjRb81axBs22OMTzmJ6iUAoedXdC-2L9DEl0EsMOavvVY_SdKstWB_tONm7445lJMlnbNERmWFrzuV66vonaNzwg95WMkyL31jYkGlE4lWvO7BsQIgp8EoXTIKCN86nii3EkRxWZ6Rnq3k0w5e8f64xYN7kGmpypCjWW2Dtg'
-token = 'eyJ0eXAiOiJKV1QiLCJub25jZSI6IkFRQUJBQUFBQUFEWHpaM2lmci1HUmJEVDQ1ek5TRUZFWlBaR1YzYXFTcWM2czFtMXJsV3hsdWJBNjVkdXZtd1BOVEhlYkJ5VTNOb2RZZldyYmpnQmExT2tHXzNGT2pvakRfY0VhaV9tT1ZzU24zQUVVVlluc1NBQSIsImFsZyI6IlJTMjU2IiwieDV0IjoiN19adWYxdHZrd0x4WWFIUzNxNmxValVZSUd3Iiwia2lkIjoiN19adWYxdHZrd0x4WWFIUzNxNmxValVZSUd3In0.eyJhdWQiOiJodHRwczovL2dyYXBoLm1pY3Jvc29mdC5jb20iLCJpc3MiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC83MmY5ODhiZi04NmYxLTQxYWYtOTFhYi0yZDdjZDAxMWRiNDcvIiwiaWF0IjoxNTMyMzk3MjU1LCJuYmYiOjE1MzIzOTcyNTUsImV4cCI6MTUzMjQwMTE1NSwiYWNjdCI6MCwiYWNyIjoiMSIsImFpbyI6IkFVUUF1LzhJQUFBQTlJWjNNemFaTGl0WXJtdWx6ZmFJNE95NS9POUlCa0d0YXVrN1pjR0NJRTJtbWpvbUluM2VCeGYxRUhxeTBvdG9wZDZqazZWMk1aUjBINkl0U1RqaERRPT0iLCJhbXIiOlsid2lhIiwibWZhIl0sImFwcF9kaXNwbGF5bmFtZSI6IkdyYXBoIGV4cGxvcmVyIiwiYXBwaWQiOiJkZThiYzhiNS1kOWY5LTQ4YjEtYThhZC1iNzQ4ZGE3MjUwNjQiLCJhcHBpZGFjciI6IjAiLCJmYW1pbHlfbmFtZSI6IkphaW4iLCJnaXZlbl9uYW1lIjoiU2hsb2FrIiwiaW5fY29ycCI6InRydWUiLCJpcGFkZHIiOiIxMzEuMTA3LjE1OS4xNzIiLCJuYW1lIjoiU2hsb2FrIEphaW4iLCJvaWQiOiI1ZWEyMGMyYS0zYTg3LTQxOWQtYTlkYy1mNzM5ZjI2YjBkZjciLCJvbnByZW1fc2lkIjoiUy0xLTUtMjEtMjEyNzUyMTE4NC0xNjA0MDEyOTIwLTE4ODc5Mjc1MjctMzE2ODIxMDkiLCJwbGF0ZiI6IjMiLCJwdWlkIjoiMTAwMzAwMDBBQUJGRkUxOSIsInNjcCI6IkNhbGVuZGFycy5SZWFkV3JpdGUgQ29udGFjdHMuUmVhZFdyaXRlIERldmljZU1hbmFnZW1lbnRBcHBzLlJlYWQuQWxsIERldmljZU1hbmFnZW1lbnRBcHBzLlJlYWRXcml0ZS5BbGwgRGV2aWNlTWFuYWdlbWVudENvbmZpZ3VyYXRpb24uUmVhZC5BbGwgRGV2aWNlTWFuYWdlbWVudENvbmZpZ3VyYXRpb24uUmVhZFdyaXRlLkFsbCBEZXZpY2VNYW5hZ2VtZW50TWFuYWdlZERldmljZXMuUHJpdmlsZWdlZE9wZXJhdGlvbnMuQWxsIERldmljZU1hbmFnZW1lbnRNYW5hZ2VkRGV2aWNlcy5SZWFkLkFsbCBEZXZpY2VNYW5hZ2VtZW50TWFuYWdlZERldmljZXMuUmVhZFdyaXRlLkFsbCBEZXZpY2VNYW5hZ2VtZW50UkJBQy5SZWFkLkFsbCBEZXZpY2VNYW5hZ2VtZW50UkJBQy5SZWFkV3JpdGUuQWxsIERldmljZU1hbmFnZW1lbnRTZXJ2aWNlQ29uZmlnLlJlYWQuQWxsIERldmljZU1hbmFnZW1lbnRTZXJ2aWNlQ29uZmlnLlJlYWRXcml0ZS5BbGwgRGlyZWN0b3J5LkFjY2Vzc0FzVXNlci5BbGwgRGlyZWN0b3J5LlJlYWRXcml0ZS5BbGwgRmlsZXMuUmVhZFdyaXRlLkFsbCBHcm91cC5SZWFkV3JpdGUuQWxsIElkZW50aXR5Umlza0V2ZW50LlJlYWQuQWxsIE1haWwuUmVhZFdyaXRlIE1haWxib3hTZXR0aW5ncy5SZWFkV3JpdGUgTm90ZXMuUmVhZFdyaXRlLkFsbCBvcGVuaWQgUGVvcGxlLlJlYWQgcHJvZmlsZSBSZXBvcnRzLlJlYWQuQWxsIFNpdGVzLlJlYWRXcml0ZS5BbGwgVGFza3MuUmVhZFdyaXRlIFVzZXIuUmVhZEJhc2ljLkFsbCBVc2VyLlJlYWRXcml0ZSBVc2VyLlJlYWRXcml0ZS5BbGwgZW1haWwiLCJzaWduaW5fc3RhdGUiOlsia21zaSJdLCJzdWIiOiJTcjZudVdDdU9jYkN1NzJ4WGNsXzNOdUZQeFhTYXNOSmVRVHdoYjY2SXZzIiwidGlkIjoiNzJmOTg4YmYtODZmMS00MWFmLTkxYWItMmQ3Y2QwMTFkYjQ3IiwidW5pcXVlX25hbWUiOiJ0LXNoamFAbWljcm9zb2Z0LmNvbSIsInVwbiI6InQtc2hqYUBtaWNyb3NvZnQuY29tIiwidXRpIjoiMUJWUzg5Sk9zVUNwdDJQZVYxNERBQSIsInZlciI6IjEuMCIsInhtc19zdCI6eyJzdWIiOiJoU1JNQ3dzZ3hnSTFVeDNDTTAtR1Z0SDg5Q19Bbm5YWHZnMVBxMThqN3IwIn19.OShnVyrnnouBiNPI3HR7eVyI2PTxJ3d_88gZWnWt_UIw3T2KoDVihCBYke8nLcRYQUgZ1ns-YL-A1yAaYUw8P8WMBzwYiNV-Y-X8dXGnytv2t5F3pWnmX4zgnfKhKwfS3GlzKf5vysv5dwf2pyJeNU26tecrywnutKmOb5WMcD-gO0cb4v-etqH7e0958g68kXZ5BMjZ4FrxzpPD2n6cmdyh6pGRj3mtETNr-IJxUBWAiE9zsqnMbbyFJkB2dbZH7fCckTRC9Mxs5lNxVU_DWqudrehtnv6iu1tCoGXoCROLq2NK-x8uPO7PZH48kh7oc7Olkv0TxO7sNrBkyYBr3Q'
+token = 'eyJ0eXAiOiJKV1QiLCJub25jZSI6IkFRQUJBQUFBQUFEWHpaM2lmci1HUmJEVDQ1ek5TRUZFdlZBazRxUXlkVGE0SXd1VGIwVVVvVHVPemdfWDg0VDh4US1oTklHODNzQnZkZDN6cW9fN19kWGlFQ0Q0TVhwVHhySU93d1o1OC1UWFRTVE1BcU5xN1NBQSIsImFsZyI6IlJTMjU2IiwieDV0IjoiN19adWYxdHZrd0x4WWFIUzNxNmxValVZSUd3Iiwia2lkIjoiN19adWYxdHZrd0x4WWFIUzNxNmxValVZSUd3In0.eyJhdWQiOiJodHRwczovL2dyYXBoLm1pY3Jvc29mdC5jb20iLCJpc3MiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC83MmY5ODhiZi04NmYxLTQxYWYtOTFhYi0yZDdjZDAxMWRiNDcvIiwiaWF0IjoxNTMyNDczOTA1LCJuYmYiOjE1MzI0NzM5MDUsImV4cCI6MTUzMjQ3NzgwNSwiYWNjdCI6MCwiYWNyIjoiMSIsImFpbyI6IkFVUUF1LzhJQUFBQUV2a0lDQ0orTVh0c2VzbWpNMGY2b29qZnRDcUw0cytLM2NNY2JHYWFsWXlCUGF5bFovSmlCelI3cHZaajdhQ1NWb1llZXRnNUJIQ2kwMDlZRVBtKzlBPT0iLCJhbXIiOlsid2lhIiwibWZhIl0sImFwcF9kaXNwbGF5bmFtZSI6IkdyYXBoIGV4cGxvcmVyIiwiYXBwaWQiOiJkZThiYzhiNS1kOWY5LTQ4YjEtYThhZC1iNzQ4ZGE3MjUwNjQiLCJhcHBpZGFjciI6IjAiLCJmYW1pbHlfbmFtZSI6IkphaW4iLCJnaXZlbl9uYW1lIjoiU2hsb2FrIiwiaW5fY29ycCI6InRydWUiLCJpcGFkZHIiOiIxNjcuMjIwLjEwMy40NyIsIm5hbWUiOiJTaGxvYWsgSmFpbiIsIm9pZCI6IjVlYTIwYzJhLTNhODctNDE5ZC1hOWRjLWY3MzlmMjZiMGRmNyIsIm9ucHJlbV9zaWQiOiJTLTEtNS0yMS0yMTI3NTIxMTg0LTE2MDQwMTI5MjAtMTg4NzkyNzUyNy0zMTY4MjEwOSIsInBsYXRmIjoiMyIsInB1aWQiOiIxMDAzMDAwMEFBQkZGRTE5Iiwic2NwIjoiQ2FsZW5kYXJzLlJlYWRXcml0ZSBDb250YWN0cy5SZWFkV3JpdGUgRGV2aWNlTWFuYWdlbWVudEFwcHMuUmVhZC5BbGwgRGV2aWNlTWFuYWdlbWVudEFwcHMuUmVhZFdyaXRlLkFsbCBEZXZpY2VNYW5hZ2VtZW50Q29uZmlndXJhdGlvbi5SZWFkLkFsbCBEZXZpY2VNYW5hZ2VtZW50Q29uZmlndXJhdGlvbi5SZWFkV3JpdGUuQWxsIERldmljZU1hbmFnZW1lbnRNYW5hZ2VkRGV2aWNlcy5Qcml2aWxlZ2VkT3BlcmF0aW9ucy5BbGwgRGV2aWNlTWFuYWdlbWVudE1hbmFnZWREZXZpY2VzLlJlYWQuQWxsIERldmljZU1hbmFnZW1lbnRNYW5hZ2VkRGV2aWNlcy5SZWFkV3JpdGUuQWxsIERldmljZU1hbmFnZW1lbnRSQkFDLlJlYWQuQWxsIERldmljZU1hbmFnZW1lbnRSQkFDLlJlYWRXcml0ZS5BbGwgRGV2aWNlTWFuYWdlbWVudFNlcnZpY2VDb25maWcuUmVhZC5BbGwgRGV2aWNlTWFuYWdlbWVudFNlcnZpY2VDb25maWcuUmVhZFdyaXRlLkFsbCBEaXJlY3RvcnkuQWNjZXNzQXNVc2VyLkFsbCBEaXJlY3RvcnkuUmVhZFdyaXRlLkFsbCBGaWxlcy5SZWFkV3JpdGUuQWxsIEdyb3VwLlJlYWRXcml0ZS5BbGwgSWRlbnRpdHlSaXNrRXZlbnQuUmVhZC5BbGwgTWFpbC5SZWFkV3JpdGUgTWFpbGJveFNldHRpbmdzLlJlYWRXcml0ZSBOb3Rlcy5SZWFkV3JpdGUuQWxsIG9wZW5pZCBQZW9wbGUuUmVhZCBwcm9maWxlIFJlcG9ydHMuUmVhZC5BbGwgU2l0ZXMuUmVhZFdyaXRlLkFsbCBUYXNrcy5SZWFkV3JpdGUgVXNlci5SZWFkQmFzaWMuQWxsIFVzZXIuUmVhZFdyaXRlIFVzZXIuUmVhZFdyaXRlLkFsbCBlbWFpbCIsInNpZ25pbl9zdGF0ZSI6WyJrbXNpIl0sInN1YiI6IlNyNm51V0N1T2NiQ3U3MnhYY2xfM051RlB4WFNhc05KZVFUd2hiNjZJdnMiLCJ0aWQiOiI3MmY5ODhiZi04NmYxLTQxYWYtOTFhYi0yZDdjZDAxMWRiNDciLCJ1bmlxdWVfbmFtZSI6InQtc2hqYUBtaWNyb3NvZnQuY29tIiwidXBuIjoidC1zaGphQG1pY3Jvc29mdC5jb20iLCJ1dGkiOiJBMUFQcldjcVlFQzRFWWI1a2pVREFBIiwidmVyIjoiMS4wIiwieG1zX3N0Ijp7InN1YiI6ImhTUk1Dd3NneGdJMVV4M0NNMC1HVnRIODlDX0FublhYdmcxUHExOGo3cjAifX0.UWh-EhI9NQ1QjgHsYgB3b1d6B5TGr3uNdLHjGnDNKEKpLESSkwQ9A29AQ058-F6Ve6sxwzOU2lNxrYiKoZu6AGlafqABRaJ6JbpD-KJfIVaZ1RLOcopshOC9SIdrIlvxNudUkKihvBF1cfCzK3RWJLAxB7j4MFlO4pS7mnligy6t8XyAqDCPcmW-QMHPkB_kpfmfw4mt6Wad6axgT4LlY1FD5pJdmj6OpbzF81FqLv_tmIwhTUDAdhuwUU1ga9NLYXSSNSPTX7K_IYRSOYTBA_glRO9hqy7bs16_uY3T1t53VDHoXadKcsGE1Ou7j4uIWD1wo701SAQ9mf40zZ57pg'
 graph_endpoint = 'https://graph.microsoft.com/v1.0{0}'
 app = Flask(__name__)
 months = {'january': 1, 'february': 2, 'march': 3, 'april': 4, 'may': 5, 'june': 6, 'july': 7, 'august': 8, 'september': 9, 'october': 10, 'november': 11, 'december': 12}
@@ -13,7 +12,9 @@ months = {'january': 1, 'february': 2, 'march': 3, 'april': 4, 'may': 5, 'june':
 def make_api_call(method, url, token, payload = None, parameters = None):
   # Send these headers with all API calls
   headers = { 'Authorization' : 'Bearer {0}'.format(token),
-              'Accept' : 'application/json' }
+              'Accept' : 'application/json',
+              'Prefer': 'outlook.timezone="Pacific Standard Time"',
+              'Prefer': 'outlook.body-content-type="Text"' }
 
   # Use these headers to instrument calls. Makes it easier
   # to correlate requests and responses in case of problems
@@ -54,20 +55,21 @@ def get_events(date, num_days=1):
 
   if (r.status_code == requests.codes.ok):
     json_response = r.json()["value"]
-    events = [{'subject': e['subject'], 'location': e['location']['displayName'], 'details': e['bodyPreview'], 'start': e['start']['dateTime'], 'end': e['end']['dateTime'],
-    "attendees": [{'name': a['emailAddress']['name']} for a in e['attendees']]} for e in json_response if e['responseStatus']['response'] in accepted_response]
-    return json.dumps(events).encode('utf-8')
+    events = [{'subject': e['subject'].encode('utf-8'), 'location': e['location']['displayName'].encode('utf-8'), 'details': e['bodyPreview'], \
+    'start': (datetime.strptime(e['start']['dateTime'][:-8], '%Y-%m-%dT%H:%M:%S') + timedelta(hours=-7)).strftime("%Y-%m-%d %H:%M:%S"), \
+    'end': (datetime.strptime(e['end']['dateTime'][:-8], '%Y-%m-%dT%H:%M:%S') + timedelta(hours=-7)).strftime("%Y-%m-%d %H:%M:%S"), \
+    "attendees": [{'name': a['emailAddress']['name'].encode('utf-8')} for a in e['attendees']]} for e in json_response if e['responseStatus']['response'] in accepted_response]
+    return events
   else:
     return "{0}: {1}".format(r.status_code, r.text)
 
 @app.route('/', methods = ['POST'])
 # takes in json body of intents
-# does task at midnight on day of deadline for duration minutes
-# add event to calendar
+# add event to calendar when available during working hours
 def api_root():
   post_data = request.json
   if post_data['intent'] == 'add-task':
-      subject, start, duration = None, None, None
+      subject, deadline, duration = None, None, None
       for e in post_data['entities']:
         if 'Calendar.Subject' in e:
             subject = e['Calendar.Subject']
@@ -79,24 +81,51 @@ def api_root():
             else:
                 for month in months:
                     if month in e["builtin.datetimeV2.date"]:
-                        start = datetime(2018, months[month], int(''.join(filter(str.isdigit, e["builtin.datetimeV2.date"]))))
-      if subject and start and duration:
-          print(subject, start, start + timedelta(minutes=duration))
+                        deadline = datetime(2018, months[month], int(''.join(filter(str.isdigit, e["builtin.datetimeV2.date"]))), 17, 00)
+      if subject and deadline and duration:
+          print(subject, deadline, deadline + timedelta(minutes=duration))
           get_messages_url = graph_endpoint.format('/me/events')
-
-          event = {"subject": subject, "start": {"dateTime": start.strftime('%Y-%m-%dT%H:%M:%S'), "timeZone": "Pacific Standard Time"}, "end": {
-              "dateTime": (start + timedelta(minutes=duration)).strftime('%Y-%m-%dT%H:%M:%S'),
+          time = find_open_time(deadline, duration)
+          event = {"subject": subject, "start": {"dateTime": time.strftime('%Y-%m-%dT%H:%M:%S'), "timeZone": "Pacific Standard Time"}, "end": {
+              "dateTime": (time + timedelta(minutes=duration)).strftime('%Y-%m-%dT%H:%M:%S'),
               "timeZone": "Pacific Standard Time"
           }}
           r = make_api_call('POST', get_messages_url, token, payload=event)
-          if (r.status_code == requests.codes.ok):
-            resp = Response(json.dumps({"txt-response": "Task successfully added!"}), status=200, mimetype='application/json')
+          if (r.status_code == requests.codes.ok or r.status_code == 201):
+            resp = Response(json.dumps({"txt-response": "Task successfully added at {}".format(time)}), status=200, mimetype='application/json')
             return resp
           else:
             print("{0}: {1}".format(r.status_code, r.text))
   resp = Response(json.dumps({"txt-response": "Failed to add task."}), status=200, mimetype='application/json')
   return resp
 
+def find_open_time(deadline, duration):
+    date_now = (datetime.now() + timedelta(minutes=60)).replace(microsecond=0,second=0,minute=0)
+    #print((deadline - date_now).days)
+    all_events = get_events(date_now, (deadline - date_now).days)
+    listings = [True] * (((deadline - date_now).days * 48) + (deadline - date_now).seconds // 60 // 30)
+    #print('len: ', len(listings))
+    for e in all_events:
+        start = datetime.strptime(e['start'], "%Y-%m-%d %H:%M:%S")
+        end = datetime.strptime(e['end'], "%Y-%m-%d %H:%M:%S")
+        #print(e['subject'], start, e['start'])
+        #print('start ind', (start - date_now).days * 48 + ((start - date_now).seconds // 60 // 30))
+        #print('end ind', (end - date_now).days * 48 + ((end - date_now).seconds // 60 // 30) + 1)
+        for i in range((start - date_now).days * 48 + ((start - date_now).seconds // 60 // 30), (end - date_now).days * 48 + ((end - date_now).seconds // 60 // 30) + 1):
+            listings[i] = False
+    for i in range(len(listings)):
+        if is_valid(listings, i, duration, date_now):
+            #print('ret ind', i)
+            return date_now + timedelta(minutes=30*i)
+
+def is_valid(listings, index, duration, date_now):
+    return all([listings[i] for i in range(index, index + duration // 30 + 2)]) and (date_now + timedelta(minutes=30*index)).weekday() < 5 and (date_now + timedelta(minutes=30*index)).hour > 9 \
+     and (date_now + timedelta(minutes=30*index)).hour < 17  
+
+
+
 if __name__ == '__main__':
     app.run()
 
+#date = datetime.now()
+#print(get_events(date))
